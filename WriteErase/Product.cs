bdc66a8f8bdae7f61688a946bb51057bbf102357
ЛедровЕИ <@106-12>
2023-03-21 15:33:17 +0300

    Second commit

--- WriteErase/Product.cs
+++ WriteErase/Product.cs
@@ -24,14 +24,14 @@ namespace WriteErase
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
         public int ProductCategory { get; set; }
-        public byte[] ProductPhoto { get; set; }
+        public string ProductPhoto { get; set; }
         public int ProductManufacturer { get; set; }
         public int ProductProvider { get; set; }
         public decimal ProductCost { get; set; }
         public Nullable<byte> ProductDiscountAmount { get; set; }
         public int ProductQuantityInStock { get; set; }
         public string ProductStatus { get; set; }
-        public int ProviderUnit { get; set; }
+        public int ProductUnit { get; set; }
     
         public virtual Manufacturer Manufacturer { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
